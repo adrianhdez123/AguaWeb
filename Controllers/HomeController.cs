56682f983557f4d1407d35084a02051884509d4a
xtuxur <alex.delzar@gmail.com>
2018-02-02 09:14:33 -0600

    Primer commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Agua.Models;
+
+namespace Agua.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Calcular(DateTime ProximoCumple, int minutos)
+        {
+            /// Aquí tienen que hacer todo
+            /// La fecha que y los minutos vienen del formulario 
+            /// Investigar Model Binding 
+
+
+            ViewBag.Dias = 0;
+            ViewBag.Botellas = 0;
+
+            return View();
+        }
+
+    }
+}
---
+++ Models/AguaViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Agua.Models
+{
+    public class AguaViewModel
+    {
+        public DateTime ProximoCumple { get; set; }
+
+       public int minutos { get; set; }
+    }
+}
